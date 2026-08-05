e644071f5f44a346991fe57b285f7336443a0c6b
Primus2k4 <duongnguyendang02@gmail.com>
2026-08-05 09:28:24 +0700

    feat: add abstract Question base class

--- include/TF.h
+++ include/TF.h
@@ -2,8 +2,25 @@
 #define TF_H
 
 #include "Question.h"
+#include <string>
 
 class TF : public Question {
+public:
+    TF(int id,
+        const std::string& prompt,
+        int points,
+        bool correctAnswer);
+
+    ~TF() override = default;
+    void display() const override;
+
+    bool checkAnswer(const std::string& answer) const override;
+    std::string getType() const override;
+    bool getCorrectAnswer() const;
+    void setCorrectAnswer(bool correctAnswer);
+
+private:
+    bool m_correctAnswer;
 };
 
-#endif
+#endif 
