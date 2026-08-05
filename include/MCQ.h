8ca93acd02649691ba12160780ebe479fe766747
Primus2k4 <duongnguyendang02@gmail.com>
2026-08-05 09:28:24 +0700

    question type header files

--- include/MCQ.h
+++ include/MCQ.h
@@ -2,8 +2,29 @@
 #define MCQ_H
 
 #include "Question.h"
+#include <string>
 
 class MCQ : public Question {
+public:
+    MCQ(int id,
+        const std::string& prompt,
+        int points,
+        const std::string options[4],
+        char correctOption);
+
+    ~MCQ() override = default;
+    void display() const override;
+
+    bool checkAnswer(const std::string& answer) const override;
+    std::string getType() const override;
+    std::string getOption(int index) const; 
+    char getCorrectOption() const;
+    bool setOption(int index, const std::string& text); 
+    bool setCorrectOption(char option);                 
+
+private:
+    std::string m_options[4];  
+    char m_correctOption; 
 };
 
-#endif
+#endif 
--- include/Question.h
+++ include/Question.h
@@ -1,9 +1,26 @@
 #ifndef QUESTION_H
 #define QUESTION_H
 
+#include <string>
+
 class Question {
 public:
-    virtual ~Question();
+    Question(int id, const std::string& prompt, int points);
+
+    virtual ~Question() = default;
+    virtual void display() const = 0;
+    virtual bool checkAnswer(const std::string& answer) const = 0;
+    virtual std::string getType() const = 0;
+    int getId() const;
+    std::string getPrompt() const;
+    int getPoints() const;
+    bool setPrompt(const std::string& prompt);   
+    bool setPoints(int points);                  
+
+private:
+    int m_id;      
+    std::string m_prompt;   
+    int m_points;   
 };
 
 #endif
