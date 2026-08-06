70449a65cfb917db6408e0c4bf6e33352ae65623
nguyenbaolong06072004-droid <nguyenbaolong06072004@gmail.com>
2026-08-06 07:30:49 +0700

    Update InputValidator.h

--- include/InputValidator.h
+++ include/InputValidator.h
@@ -1,7 +1,13 @@
 #ifndef INPUTVALIDATOR_H
 #define INPUTVALIDATOR_H
 
+#include <string>
+
 class InputValidator {
+public:
+    static bool isPositiveInt(const std::string& str);
+    static bool isValidChoice(char choice, char minChoice, char maxChoice);
+    static bool isNonEmptyString(const std::string& str);
 };
 
 #endif
