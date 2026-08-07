859d9aca1a7de22aff31f329b0b62931574d4453
MT30112005 <ytung466@gmail.com>
2026-08-07 08:23:27 +0700

    Update QuizEngine.h

--- include/QuizEngine.h
+++ include/QuizEngine.h
@@ -1,7 +1,67 @@
 #ifndef QUIZENGINE_H
 #define QUIZENGINE_H
 
-class QuizEngine {
+#include <string>
+#include <iostream>
+
+#include "Quiz.h"
+#include "QuestionBank.h"
+#include "AttemptState.h"
+
+using namespace std;
+
+class QuizEngine
+{
+private:
+
+    Quiz* currentQuiz;
+
+    QuestionBank* questionBank;
+
+    string answers[30];
+
+    int currentIndex;
+
+    int score;
+
+    AttemptState state;
+
+public:
+
+    // Constructor
+    QuizEngine();
+
+    // Start Quiz
+    bool start(Quiz* quiz, QuestionBank* bank);
+
+    // Display
+    void displayCurrentQuestion() const;
+
+    // Answer
+    bool answer(const string& ans);
+
+    // Navigation
+    bool next();
+
+    bool previous();
+
+    // Submit
+    bool submit();
+
+    // Calculate score
+    int calculateScore();
+
+    // Show result
+    void showResult() const;
+
+    // Getter
+    AttemptState getState() const;
+
+    int getCurrentIndex() const;
+
+    int getScore() const;
+
+    string getCurrentAnswer() const;
 };
 
 #endif
