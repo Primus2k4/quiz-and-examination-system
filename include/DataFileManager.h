1c30da4fe9c7bc58492be22b35af0a70050f41dd
nguyenbaolong06072004-droid <nguyenbaolong06072004@gmail.com>
2026-08-08 07:14:32 +0700

    Update DataFileManager.h

--- include/DataFileManager.h
+++ include/DataFileManager.h
@@ -8,7 +8,7 @@ class DataFileManager {
 public:
     static bool loadQuestions(QuestionBank& bank, const std::string& filepath);
     static bool saveQuestions(const QuestionBank& bank, const std::string& filepath);
-    static bool loadQuizzes(QuizManager& manager, const std::string& filepath);
+    static bool loadQuizzes(QuizManager& manager, const QuestionBank& bank, const std::string& filepath);
     static bool saveQuizzes(const QuizManager& manager, const std::string& filepath);
 };
 #endif
