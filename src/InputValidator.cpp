66f0ad3e3853df5c1e77150e87611f5c5ba9a48b
nguyenbaolong06072004-droid <nguyenbaolong06072004@gmail.com>
2026-08-08 07:16:29 +0700

    Update InputValidator.cpp

--- src/InputValidator.cpp
+++ src/InputValidator.cpp
@@ -6,6 +6,7 @@ bool InputValidator::isPositiveInt(const std::string& str) {
     for (int i = 0; i < (int)str.size(); i++) {
         if (!isdigit(str[i])) return false;
     }
+    if (str.find_first_not_of('0') == std::string::npos) return false;
     return true;
 }
 
